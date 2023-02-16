a2941a436137a4adaa4d92ad06de028b1fb54fa1
Pozitronik <antikillerxxl@gmail.com>
2023-02-16 12:36:15 +0400

    Module config in tests

--- tests/_app/config/history.php
+++ tests/_app/config/history.php
@@ -1,5 +1,14 @@
 <?php
 declare(strict_types = 1);
 
-return [];
+use app\models\Users;
+use cusodede\history\models\active_record\History;
+
+return [
+	'class' => History::class,
+	'params' => [
+		'userIdentityClass' => Users::class,
+		'viewPath' =>  './src/views/history'
+	]
+];
 
